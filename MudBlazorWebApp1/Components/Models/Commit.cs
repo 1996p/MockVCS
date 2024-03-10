2ee70b4dc98284e853204d3eff4057d8c2485fd3
1996p <dursinear@gmail.com>
2024-03-10 23:00:39 +0700

    добавлен коммит, можно изменять проекты

---
+++ MudBlazorWebApp1/Components/Models/Commit.cs
@@ -0,0 +1,16 @@
+﻿namespace MudBlazorWebApp1.Components.Models
+{
+    public class Commit
+    {
+        public string Message { get; set; }
+        public DateTime DateTime { get; set; }
+        public string SourceCode { get; set; } = String.Empty;
+        public Commit() { }
+        public Commit(string message, string sourceCode) 
+        {
+            Message = message;
+            DateTime = DateTime.Now;
+            SourceCode = sourceCode;
+        }
+    }
+}
--- MudBlazorWebApp1/Components/Models/Project.cs
+++ MudBlazorWebApp1/Components/Models/Project.cs
@@ -1,24 +1,33 @@
-﻿namespace ooap_lab_1.Models
+﻿using MudBlazorWebApp1.Components.Models;
+using System.Collections.Generic;
+
+namespace ooap_lab_1.Models
 {
     public class Project : ICopyable<Project>
     {
         public int Id { get; set; }
         public string Name { get; set; } = String.Empty;
-        public DateTime LastUpdateTime { get;set; }
-        public string SourceCode { get; set; } = String.Empty;
+        public List<Commit> Commits { get; set; } = new();
 
+        public Project()
+        {
+           
+        }
 
-        public Project(int id, string name, DateTime lastTimeUpdate, string sourceCode)
+        public Project(int id, string name, List<Commit> commits)
         {
             Id = id;
             Name = name;
-            LastUpdateTime = lastTimeUpdate;
-            SourceCode = sourceCode;
+            
+            foreach(Commit commit in commits)
+            {
+                Commits.Add(new Commit(commit.Message, commit.SourceCode));
+            }
         }
 
         public Project Copy()
         {
-            return new Project(Id, Name, LastUpdateTime, SourceCode);
+            return new Project(Id, Name, Commits);
         }
     }
 }
